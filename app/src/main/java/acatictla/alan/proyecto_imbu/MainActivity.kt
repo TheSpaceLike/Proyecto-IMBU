a23c1c92158594f5ec9dfc6a98f6566348335637
Alan A.M <spacelike01@gmail.com>
2020-03-31 00:07:34 -0700

    Subiendo Repositorio

---
+++ app/src/main/java/acatictla/alan/proyecto_imbu/MainActivity.kt
@@ -0,0 +1,12 @@
+package acatictla.alan.proyecto_imbu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
