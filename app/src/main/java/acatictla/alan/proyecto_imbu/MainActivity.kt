ade43a35427f753f1ac88dd345b0a4af6ddb6cb4
Alan A.M <spacelike01@gmail.com>
2020-04-30 13:43:47 -0600

    New pantallas regristrar_usuario/registrar_confirmacion & correccion de errores

--- app/src/main/java/acatictla/alan/proyecto_imbu/MainActivity.kt
+++ app/src/main/java/acatictla/alan/proyecto_imbu/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_iniciar)
+        setContentView(R.layout.activity_registrar_usuario)
     }
 }
