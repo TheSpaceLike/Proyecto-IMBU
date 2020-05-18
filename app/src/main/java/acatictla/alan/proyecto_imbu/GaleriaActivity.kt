b1996665fc57810452957f1ea4e2a277d996c85f
Alan A.M <spacelike01@gmail.com>
2020-05-18 02:13:49 -0600

    Subiendo nuevas activitys

---
+++ app/src/main/java/acatictla/alan/proyecto_imbu/GaleriaActivity.kt
@@ -0,0 +1,11 @@
+package acatictla.alan.proyecto_imbu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GaleriaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/acatictla/alan/proyecto_imbu/LoginIniciar.kt
@@ -0,0 +1,11 @@
+package acatictla.alan.proyecto_imbu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginIniciar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
